1a3f5923a6fca02d14b2195c41e5720d589c49c5
Julie Laursen <julielaursen1@gmail.com>
2025-04-27 16:09:58 -0500

    fix vscode errors

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // Optional: you can add event listeners here later if needed
+    },
+  },
+})
