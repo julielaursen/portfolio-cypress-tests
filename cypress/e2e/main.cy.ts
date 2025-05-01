844140e29949ecf81df1a14e75ce2a23248b3d8a
Julie Laursen <julielaursen1@gmail.com>
2025-05-01 11:46:30 -0500

    wip

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -16,8 +16,12 @@ describe("Main Page", () => {
     cy.get("div.grid-container").find(".grid-item").should("have.length", 6)
   })
 
-  // it("Verify search box functionality", () => {
-  //   cy.get("input[type='search']").type("Cypress")
-  //   cy.contains("Cypress").should("exist")
-  // })
+  it.skip("Verify search box functionality", () => {
+    cy.get('[data-testid="tab-1"]').should("have.class", "active")
+    cy.get('input[type="search"]').type("Cypress")
+    cy.get('[data-testid="tab-2"]').should("have.class", "active")
+    cy.contains("Cypress").should("be.visible")
+    cy.get("input[type='search']").clear()
+    cy.get('[data-testid="tab-1"]').should("have.class", "active")
+  })
 })
