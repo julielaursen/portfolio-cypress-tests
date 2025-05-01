f7adcbe239cb541d7c0191e047f0c3350d758875
Julie Laursen <julielaursen1@gmail.com>
2025-05-01 09:57:45 -0500

    update main cy ts

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -6,4 +6,18 @@ describe("Main Page", () => {
   it("Verify page title is correct", () => {
     cy.contains("h1", "Julie Coleman")
   })
+
+  it("Verify introduction", () => {
+    cy.contains("h2", "Introduction")
+    cy.get("div.introduction").find("p").should("have.length", 3)
+  })
+
+  it("Verify grid and its items", () => {
+    cy.get("div.grid-container").find(".grid-item").should("have.length", 6)
+  })
+
+  // it("Verify search box functionality", () => {
+  //   cy.get("input[type='search']").type("Cypress")
+  //   cy.contains("Cypress").should("exist")
+  // })
 })
