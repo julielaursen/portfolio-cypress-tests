776f84f4c7f5e3c9e546e12998b829bb90ec6d20
Julie Laursen <julielaursen1@gmail.com>
2025-05-02 09:55:13 -0500

    add dataCy command

--- cypress/e2e/main.cy.ts
+++ cypress/e2e/main.cy.ts
@@ -16,12 +16,12 @@ describe("Main Page", () => {
     cy.get("div.grid-container").find(".grid-item").should("have.length", 6)
   })
 
-  it.skip("Verify search box functionality", () => {
-    cy.get('[data-testid="tab-1"]').should("have.class", "active")
+  it("Verify search box functionality", () => {
+    cy.dataCy("skills-tab-dev").should("have.class", "active")
     cy.get('input[type="search"]').type("Cypress")
-    cy.get('[data-testid="tab-2"]').should("have.class", "active")
+    cy.dataCy("skills-tab-qa").should("have.class", "active")
     cy.contains("Cypress").should("be.visible")
     cy.get("input[type='search']").clear()
-    cy.get('[data-testid="tab-1"]').should("have.class", "active")
+    cy.dataCy("skills-tab-dev").should("have.class", "active")
   })
 })
