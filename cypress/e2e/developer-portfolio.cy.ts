ab6e2180806c477ee0eb83e7f837df7f8f8874af
Julie Laursen <julielaursen1@gmail.com>
2025-05-04 23:20:38 -0500

    add more developer tests

--- cypress/e2e/developer-portfolio.cy.ts
+++ cypress/e2e/developer-portfolio.cy.ts
@@ -32,4 +32,85 @@ describe("Developer Portfolio Page Tests", () => {
     cy.get(".close-btn").click()
     cy.get(".overlay").should("not.be.visible")
   })
+
+  it("Should display the GitHub calendar", () => {
+    // Check the GitHub calendar container
+    cy.get(".calendar").should("be.visible")
+  })
+
+  it("Should display GitHub stats, most used languages, and streak stats", () => {
+    // Check GitHub stats
+    cy.get(".github-stats img")
+      .should("be.visible")
+      .and("have.attr", "src")
+      .and("include", "github-readme-stats.vercel.app/api")
+
+    // Check most used languages
+    cy.get(".most-used-languages img")
+      .should("be.visible")
+      .and("have.attr", "src")
+      .and("include", "github-readme-stats.vercel.app/api/top-langs")
+
+    // Check GitHub streak stats
+    cy.get(".github-streak img")
+      .should("be.visible")
+      .and("have.attr", "src")
+      .and("include", "streak-stats.demolab.com")
+  })
+
+  it("Should display the repositories section with correct links", () => {
+    cy.get(".gitub-repos h2").should(
+      "contain.text",
+      "My Github Repositories for Automation Tests"
+    )
+
+    // Check the first repository link
+    cy.get(".grid-container .grid-item")
+      .first()
+      .find("a")
+      .should(
+        "have.attr",
+        "href",
+        "https://github.com/julielaursen/portfolio-cypress-tests"
+      )
+      .and("have.attr", "target", "_blank")
+
+    // Check the second repository link
+    cy.get(".grid-container .grid-item")
+      .eq(1)
+      .find("a")
+      .should(
+        "have.attr",
+        "href",
+        "https://github.com/julielaursen/portfolio-playwright-tests"
+      )
+      .and("have.attr", "target", "_blank")
+  })
+
+  it("Should display the footer with social media icons and sitemap", () => {
+    // Check social media icons
+    cy.get(".social-media-icons img").should("have.length", 3)
+
+    // Check GitHub icon
+    cy.get(".social-media-icons img")
+      .first()
+      .should("have.attr", "alt", "GitHub Logo")
+      .and("have.attr", "src", "../images/Github_black.png")
+
+    // Check sitemap links
+    cy.get(".sitemap ul li a").should("have.length", 4)
+    cy.get(".sitemap ul li a")
+      .first()
+      .should("contain.text", "Marketing Portfolio")
+  })
+
+  it("Should display the contact information in the footer", () => {
+    // Check contact email
+    cy.get(".footer-email a")
+      .should("be.visible")
+      .and("have.attr", "href", "mailto:julielaursen1@gmail.com")
+
+    // Check copyright text
+    cy.get(".copy").should("contain.text", "2025 © Julie Coleman")
+  })
 })
