8b68a807dfb9c1166049e2e3a9671b69b0364325
Julie Laursen <julielaursen1@gmail.com>
2025-06-08 22:30:03 -0500

    update tests

--- cypress/e2e/marketing/marketing-other.cy.ts
+++ cypress/e2e/marketing/marketing-other.cy.ts
@@ -1,16 +1,16 @@
 describe("Marketing Portfolio Page Tests", () => {
   beforeEach(() => {
-    cy.visit("portfolios/marketing-portfolio.html")
+    cy.visit("portfolios/marketing-other.html")
   })
 
   it("Should display the header with the logo and navigation menu", () => {
     cy.get(".logo")
       .should("be.visible")
       .and("have.attr", "src", "../images/logo1.png")
+  })
 
-    it("Verify menu button calls overlay", () => {
-      cy.checkMenuOverlay()
-    })
+  it("Verify menu button calls overlay", () => {
+    cy.checkMenuOverlay()
   })
 
   it("verify footer links", () => {
