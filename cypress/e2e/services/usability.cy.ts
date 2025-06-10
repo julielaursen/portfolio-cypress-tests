1c151582477a8b23c9949e662cbab19e60fb3fa6
Julie Laursen <julielaursen1@gmail.com>
2025-06-09 23:11:13 -0500

    update usability tests

--- cypress/e2e/services/usability.cy.ts
+++ cypress/e2e/services/usability.cy.ts
@@ -16,12 +16,23 @@ describe("Services Page Tests", () => {
   it("verify footer links", () => {
     cy.checkFooter()
     cy.contains("Marketing Portfolio").click({ force: true })
-    cy.url().should("include", "portfolios/marketing-portfolio.html")
+    cy.url().should(
+      "eq",
+      "https://julielaursen.github.io/portfolios/marketing-portfolio.html"
+    )
     cy.go("back")
     cy.contains("UI/UX Portfolio").click({ force: true })
+    cy.url().should(
+      "eq",
+      "https://julielaursen.github.io/portfolios/uiux-portfolio.html"
+    )
     cy.go("back")
     cy.contains("Developer Portfolio").click({ force: true })
-    cy.url().should("include", "portfolios/developer-portfolio.html")
+    cy.contains("Developer Portfolio").click({ force: true })
+    cy.url().should(
+      "eq",
+      "https://julielaursen.github.io/portfolios/developer-portfolio.html"
+    )
     cy.go("back")
     cy.contains("Services").click({ force: true })
     cy.url().should("include", "services.html")
